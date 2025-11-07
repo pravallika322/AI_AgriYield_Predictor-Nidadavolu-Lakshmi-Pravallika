38561015fccbc6ca59d20f80df0772180175c385
Nidadavolu Lakshmi Pravallika <pravallika322@gmail.com>
2025-11-07 17:10:35 +0530

    Remove unnecessary debug/temp files for final submission

--- check_yield.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-pd.options.display.float_format = '{:,.2f}'.format
-
-df = pd.read_csv(r"C:\Users\nidad\OneDrive\Desktop\crop yield predictor\Merged_Crop_Yield_Dataset.csv")
-# compute mean yield by label (in kg/ha)
-group = df.groupby('label')['Yield'].agg(['count','mean','min','max']).reset_index()
-group['mean_tons'] = group['mean'] / 1000.0
-group = group.sort_values('mean', ascending=False)
-print(group.head(40))
